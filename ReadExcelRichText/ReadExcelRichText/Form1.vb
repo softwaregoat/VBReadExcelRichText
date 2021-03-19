56b320b667261ead2a0dbe2cdfd9b5e86cc8bf9a
Software Goat <61810640+softwaregoat@users.noreply.github.com>
2021-03-19 23:49:36 +0800

    First comment
    First comment

---
+++ ReadExcelRichText/ReadExcelRichText/Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports Spire.Xls
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim workbook As Workbook = New Workbook()
+        workbook.LoadFromFile("D:\Project\VisualStudio\VB\VBReadExcelRichText\ReadExcelRichText\Book1.xlsx")
+        Dim sheet As Worksheet = workbook.Worksheets(0)
+        RichTextBox1.Rtf = sheet.Range("A1").RichText.RtfText
+    End Sub
+End Class
